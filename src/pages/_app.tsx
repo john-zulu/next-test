4b8564345867708b12fee2b16c0053465a1e0f63
John Toro <john@zuluapp.io>
2023-09-18 14:58:10 -0500

    Test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </CacheProvider>
+  )
 }
--- src/pages/john.tsx
+++ src/pages/john.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+} from '@chakra-ui/react'
+
 
 export default function John({ time }: any) {
     return (
-        <div>John - {time}</div>
+        <div>John - {time}
+            <Alert status='error'>
+                <AlertIcon />
+                <AlertTitle>Your browser is outdated!</AlertTitle>
+                <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
+            </Alert>
+        </div>
     )
 }
 
