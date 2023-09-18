883606ab05fa9f3c1a68cc9c0d1734e52b8e2b35
John Toro <john@zuluapp.io>
2023-09-18 15:30:18 -0500

    Test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </CacheProvider>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
